a65f9884e4774c37dffe995244235eb14443530d
Nikita Palyvoda <mikitapalivoda@gmail.com>
2025-11-21 09:19:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CurrencyConvert from "./components/CurrencyConverter/CurrenycConverter.jsx"
+
+function App() {
+  return (
+    <div>
+      <CurrencyConvert></CurrencyConvert>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CurrencyConverter/CurrenycConverter.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react"
+
+import "./CurrencyConverter.scss"
+import CurrencySelector from "./CurrencySelector.jsx"
+
+
+export default function CurrencyConverter() {
+  // state of input and output currencies
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+
+  return (
+    <section className="main-section">
+      <div className="text-section">
+        <h1>Currency Converter</h1>
+        <p>
+          Check live rates, set rate alerts, receive 
+          notifications and more
+        </p>
+      </div>
+      <div className="converting-section">
+        <span>Amount</span>
+        <div className="currency-input">
+          <CurrencySelector value={fromCurrency} onChange={setFromCurrency}/>
+          <input type="number" className="block"/>
+        </div>
+      </div>
+    </section>
+  )
+}
